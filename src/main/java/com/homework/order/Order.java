7b86afab12f1701cf6433d4aa10b8c8e7c0521d7
Ani Babayan <anbabayan@vmware.com>
2023-04-20 18:15:20 +0400

    initial commit

---
+++ src/main/java/com/homework/order/Order.java
@@ -0,0 +1,67 @@
+package com.homework.order;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+//using mongoDB
+@Document
+public class Order implements Serializable {
+    @Id
+    private String orderId;
+    private String orderDate;
+    private String status;
+    private String customerName;
+    private String productName;
+
+    public Order() {
+    }
+
+    public Order(String orderDate, String status, String customerName, String productName) {
+        this.orderDate = orderDate;
+        this.status = status;
+        this.customerName = customerName;
+        this.productName = productName;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+}
---
+++ src/main/java/com/homework/order/OrderWebResource.java
@@ -0,0 +1,31 @@
+package com.homework.order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Web resource to manage the orders.
+ */
+@RestController
+@RequestMapping(path = "api/v1/orders")
+@CrossOrigin
+public class OrderWebResource implements Serializable {
+    private OrderService orderService;
+
+    @Autowired
+    OrderWebResource(OrderService productService) {
+        this.orderService = productService;
+    }
+    @GetMapping
+    @Cacheable("orders")
+    public List<Order> getOrders() {
+        return orderService.getOrders();
+    }
+}
